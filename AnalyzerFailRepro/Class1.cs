2b27bd168703d3ead0444bb1cec28f91cdabee0e
Jonathan <J.vanderGeugten@gcbiotech.com>
2022-02-28 09:51:27 +0100

    Initial commit

---
+++ AnalyzerFailRepro/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AnalyzerFailRepro
+{
+    public class Class1
+    {
+        public static void DiagnosticReported(string s)
+        {
+            Console.WriteLine(Double.Parse(s));
+        }
+
+        public static void DiagnosticNotReportedButShould(string s)
+        {
+            Console.WriteLine(Double.Parse(s.AsSpan()));
+        }
+    }
+}
